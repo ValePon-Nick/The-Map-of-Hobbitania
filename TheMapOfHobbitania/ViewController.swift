8de26d04d2926b1232c9eb205c4cddab8ad9677c
vale <Vale.ponick@gmail.com>
2025-08-14 12:28:03 +0700

    Initial Commit

---
+++ TheMapOfHobbitania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMapOfHobbitania
+//
+//  Created by Валерия Пономарева on 14.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
